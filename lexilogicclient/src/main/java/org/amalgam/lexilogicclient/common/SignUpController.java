e5ef33386a9d028634045079342247d8432efe0a
Lenar <2233894@slu.edu.ph>
2024-04-22 21:09:57 +0800

    + Initial Start of Sign Up View

---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/common/SignUpController.java
@@ -0,0 +1,20 @@
+package org.amalgam.lexilogicclient.common;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignUpController {
+
+    // Private Variables
+    @FXML
+    private AnchorPane signUpPane;
+    @FXML
+    private Button signInButton;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+
+}
---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/common/SignUpPanel.java
@@ -0,0 +1,10 @@
+package org.amalgam.lexilogicclient.common;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SignUpPanel extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
