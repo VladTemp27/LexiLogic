a8dc313cc15a4cd852128115b672981ad659aac6
Lenar <2233894@slu.edu.ph>
2024-04-22 21:09:49 +0800

    + Initial Start of Login View

---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/common/LoginController.java
@@ -0,0 +1,22 @@
+package org.amalgam.lexilogicclient.common;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    // Private Variables
+    @FXML
+    private AnchorPane loginPane;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button signUpButton;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+
+}
