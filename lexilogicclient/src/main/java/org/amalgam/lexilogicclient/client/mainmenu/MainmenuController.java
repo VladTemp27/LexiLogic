c112ade07866c099dabe402a65ae04eecaad3fd8
Georcelle <2234664@slu.edu.ph>
2024-04-24 21:01:59 +0800

    + mainmenu controller

---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/mainmenu/MainmenuController.java
@@ -0,0 +1,21 @@
+package org.amalgam.lexilogicclient.client.mainmenu;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MainmenuController {
+    //private variables
+    @FXML
+    private AnchorPane mainmenuPane;
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button leaderboardsButton;
+    @FXML
+    private Button historyButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button profileButton;
+}
