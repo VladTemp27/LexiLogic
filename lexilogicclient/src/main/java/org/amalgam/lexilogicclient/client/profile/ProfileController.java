a9b944ccfd98248845311f3ab1ea494c4ab8cad5
Georcelle <2234664@slu.edu.ph>
2024-04-24 23:26:08 +0800

    + initial profile controller class

---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/profile/ProfileController.java
@@ -0,0 +1,26 @@
+package org.amalgam.lexilogicclient.client.profile;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ProfileController {
+
+    // private variables
+
+    @FXML
+    private AnchorPane profilePane;
+    @FXML
+    private Button changePasswordButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label scoreLabel;
+}
