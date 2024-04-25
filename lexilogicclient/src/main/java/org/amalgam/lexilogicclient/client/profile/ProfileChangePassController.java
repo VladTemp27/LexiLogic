f00ae55f38ed275a4bcd4f67fda2dd2e0cb79530
Balls <MyGmail@email.com>
2024-04-25 10:11:56 +0800

    +initial controller class for profile change pass

---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/profile/ProfileChangePassController.java
@@ -0,0 +1,21 @@
+package org.amalgam.lexilogicclient.client.profile;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.layout.AnchorPane;
+
+public class ProfileChangePassController {
+// private variables
+    @FXML
+    private AnchorPane changpassPane;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private TextField oldPasswordField;
+    @FXML
+    private TextField newPasswordField;
+}
