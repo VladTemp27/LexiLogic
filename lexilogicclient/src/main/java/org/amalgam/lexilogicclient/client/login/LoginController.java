2450c7baba6be8bbe4cd1bb3cee16c12052712b7
Lenar R. Doming <2233894@slu.edu.ph>
2024-04-25 10:15:06 +0800

    + Hover Effects for LoginController

--- lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/login/LoginController.java
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/login/LoginController.java
@@ -19,4 +19,23 @@ public class LoginController {
     @FXML
     private TextField passwordField;
 
+    /**
+     * Adds hover effect to the given button.
+     *
+     * @param button The button to add hover effect to.
+     */
+    private void addHoverEffect(Button button) {
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#D9E0A2, -10%);"));
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #D9E0A2;"));
+    }
+
+    /**
+     * Initializes the controller.
+     * This method sets up the UI components and initializes the data model.
+     */
+    @FXML
+    public void initialize() {
+        addHoverEffect(loginButton);
+        addHoverEffect(signUpButton);
+    }
 }
