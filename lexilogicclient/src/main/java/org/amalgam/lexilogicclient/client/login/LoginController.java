64420af6e27ffbee250d314676904c39cf66fdb5
Lenar <2233894@slu.edu.ph>
2024-04-26 00:49:47 +0800

    + Initial Implementation of LoginController

--- lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/login/LoginController.java
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/login/LoginController.java
@@ -1,9 +1,13 @@
 package org.amalgam.lexilogicclient.client.login;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import org.amalgam.lexilogicclient.MainController;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -19,6 +23,17 @@ public class LoginController {
     @FXML
     private TextField passwordField;
 
+    private MainController mainController;
+
+    /**
+     * Sets the Main Controller.
+     *
+     * @param mainController
+     */
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
     /**
      * Adds hover effect to the given button.
      *
@@ -30,6 +45,31 @@ public class LoginController {
     }
 
     /**
+     * Shows an alert to a user if there is an error.
+     *
+     * @param message
+     */
+    private void showAlert(String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    /**
+     * Shows the signup panel when pressed.
+     */
+    @FXML
+    public void handleSignUp() {
+        if (mainController != null) {
+            mainController.loadSignUpView();
+        } else {
+            System.out.println("MainController is not set.");
+        }
+    }
+
+    /**
      * Initializes the controller.
      * This method sets up the UI components and initializes the data model.
      */
@@ -37,5 +77,39 @@ public class LoginController {
     public void initialize() {
         addHoverEffect(loginButton);
         addHoverEffect(signUpButton);
+        signUpButton.setOnAction(event -> handleSignUp());
+    }
+
+    /**
+     * Handles the Login of the user.
+     *
+     *
+     */
+    @FXML
+    public void OnLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+    }
+
+    /**
+     * Gets the objects used.
+     * This method returns a string indicating the type of objects used by the controller.
+     *
+     * @return A string representing the objects used.
+     */
+    //TODO: @Override
+    private void getObjectsUsed() {
+        //TODO: Return Value
+        //return "user";
+    }
+
+    /**
+     * Fetches and updates data remotely.
+     * This method is called to update the data displayed in the UI.
+     *
+     */
+    //TODO: @Override
+    private void fetchAndUpdate() {
+        //TODO: Fetching of Data
     }
 }
