c980796b519250ed27eb6147a470271c043b6027
Georcelle <2234664@slu.edu.ph>
2024-04-25 21:35:57 +0800

    + initial controller class for profile change username

---
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/profile/ProfileChangeUsernameController.java
@@ -0,0 +1,18 @@
+package org.amalgam.lexilogicclient.client.profile;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ProfileChangeUsernameController {
+    // private variables
+    @FXML
+    private AnchorPane changeUsernamePane;
+    @FXML
+    private Button backButton;
+    @FXML
+    private TextField newUsernameField;
+    @FXML
+    private Button saveButton;
+}
