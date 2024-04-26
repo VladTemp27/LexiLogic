32a67d98694d93c9a04a2a253245acec4e0b8a4d
Georcelle <2234664@slu.edu.ph>
2024-04-27 01:08:10 +0800

    + added methods to handle different buttons

--- lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/profile/ProfileChangePassController.java
+++ lexilogicclient/src/main/java/org/amalgam/lexilogicclient/client/profile/ProfileChangePassController.java
@@ -1,15 +1,17 @@
 package org.amalgam.lexilogicclient.client.profile;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.layout.AnchorPane;
+import org.amalgam.lexilogicclient.MainController;
 
 public class ProfileChangePassController {
 // private variables
     @FXML
-    private AnchorPane changpassPane;
+    private AnchorPane changepassPane;
     @FXML
     private Button backButton;
     @FXML
@@ -18,4 +20,77 @@ public class ProfileChangePassController {
     private TextField oldPasswordField;
     @FXML
     private TextField newPasswordField;
+    private MainController mainController;
+
+    /**
+     * Sets the Main Controller.
+     *
+     * @param mainController
+     */
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    /**
+     * Adds hover effect to the given button.
+     *
+     * @param button The button to add hover effect to.
+     */
+    private void addHoverEffect(Button button) {
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#D9E0A2, -10%);"));
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #D9E0A2;"));
+    }
+
+    /**
+     * Shows an alert to a user if there is an error.
+     *
+     * @param message
+     */
+    private void showAlert(String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    @FXML
+    public void handleSave(){
+        // handles the save button when the username is changed
+    }
+    @FXML
+    public void handleBack(){
+        // handle action to go back to profile view when pressed
+    }
+    /**
+     * Initializes the controller.
+     * This method sets up the UI components and initializes the data model.
+     */
+    @FXML
+    public void initialize() {
+        addHoverEffect(saveButton);
+        addHoverEffect(backButton);
+        saveButton.setOnAction(event -> handleSave());
+        backButton.setOnAction(event -> handleBack());
+    }
+    /**
+     * Gets the objects used.
+     * This method returns a string indicating the type of objects used by the controller.
+     *
+     * @return A string representing the objects used.
+     */
+    //TODO: @Override
+    private void getObjectsUsed() {
+        //TODO: Return Value
+        //return "user";
+    }
+
+    /**
+     * Fetches and updates data remotely.
+     * This method is called to update the data displayed in the UI.
+     *
+     */
+    //TODO: @Override
+    private void fetchAndUpdate() {
+        //TODO: Fetching of Data
+    }
 }
