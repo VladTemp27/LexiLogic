9de4780c12f41d1c738053a01addb3bbe5335dde
Marven <2234244@slu.edu.ph>
2024-05-09 20:43:01 +0800

    + FetchMatchHistoryService:   + Object call
    + LoginMicroservice:
      + Updated the parameters and the object call

--- newlexilogicclient/src/main/java/org/amalgam/backend/microservices/LoginMicroservice.java
+++ newlexilogicclient/src/main/java/org/amalgam/backend/microservices/LoginMicroservice.java
@@ -1,18 +1,18 @@
 package org.amalgam.backend.microservices;
 
+import org.amalgam.UIControllers.PlayerCallback;
 import org.amalgam.Utils.Exceptions.AlreadyLoggedInException;
 import org.amalgam.Utils.Exceptions.InvalidCredentialsException;
 import org.amalgam.Utils.Exceptions.UserExistenceException;
-import org.amalgam.Utils.UIControllers.PlayerCallback;
 import org.amalgam.backend.microservices.serverconnection.ORBConnection;
 
 public class LoginMicroservice {
 
     ORBConnection orbConnection = new ORBConnection(1099, "hostname");
 
-    public void process (PlayerCallback playerCallback) {
+    public void process (PlayerCallback playerCallback, String password) {
         try {
-            orbConnection.retrievePlayerRequestStub().login(playerCallback);
+            orbConnection.retrievePlayerRequestStub().login(playerCallback, password);
         } catch (AlreadyLoggedInException | InvalidCredentialsException | UserExistenceException e){
             throw new RuntimeException(e);
         }
