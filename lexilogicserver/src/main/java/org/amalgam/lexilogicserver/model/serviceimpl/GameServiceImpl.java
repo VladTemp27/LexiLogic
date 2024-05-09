839b023bc103303d9f7ac5faec5477543c22cfc5
Lestat <2233915@slu.edul.ph>
2024-05-09 12:19:44 +0800

    -old compiled idl java classes +syntax fix idl

--- lexilogicserver/src/main/java/org/amalgam/lexilogicserver/model/serviceimpl/GameServiceImpl.java
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/model/serviceimpl/GameServiceImpl.java
@@ -2,31 +2,41 @@ package org.amalgam.lexilogicserver.model.serviceimpl;
 
 import org.amalgam.Service.GameExceptions.*;
 import org.amalgam.Service.GameServiceModule.GameServicePOA;
+import org.amalgam.Utils.Exceptions.*;
+import org.amalgam.Utils.Exceptions.DuplicateWordException;
+import org.amalgam.Utils.Objects.GameRoom;
 import org.amalgam.Utils.Objects.Leaderboard;
 import org.amalgam.Utils.Objects.Lobby;
+import org.amalgam.Utils.UIControllers.PlayerCallback;
 
 public class GameServiceImpl extends GameServicePOA {
-    public char[][] fetchWordBox(int roomID) throws WordFetchException, InvalidRoomID {
-        return new char[0][];
-    }
-
-    public Leaderboard[] getLeaderboards() throws EmptyLeaderBoard {
-        return new Leaderboard[0];
-    }
-
-    public void verifyWord(String word) throws InvalidWordFormat, DuplicateWordException {
-
-    }
-
-    public int validateTotalPoints(String[] word) throws InsufficientWordPoints, InvalidTotalPoints {
-        return 0;
-    }
-
-    public String fetchWinner(int lobbyId) throws LobbyDoesNotExist, WinnerDoesNotExist {
+    @Override
+    public GameRoom matchMake(PlayerCallback player_callback) throws MatchCreationFailedException {
         return null;
     }
 
-    public void insertLobby(Lobby lobby) throws InvalidLobbyData {
+    @Override
+    public char[][] fetchWordBox(int roomID) throws WordFetchFailedException, InvalidRoomIDException {
+        return new char[0][];
+    }
 
+    @Override
+    public Leaderboard[] getLeaderboards() throws EmptyLeaderBoardException {
+        return new Leaderboard[0];
+    }
+
+    @Override
+    public void verifyWord(String word) throws InvalidWordFormatException, DuplicateWordException {
+
+    }
+
+    @Override
+    public int validateTotalPoints(String[] word) throws InsufficientWordPointsException, InvalidTotalPointsException {
+        return 0;
+    }
+
+    @Override
+    public String fetchWinner(int lobbyId) throws LobbyDoesNotExistException, WinnerDoesNotExistException {
+        return "";
     }
 }
