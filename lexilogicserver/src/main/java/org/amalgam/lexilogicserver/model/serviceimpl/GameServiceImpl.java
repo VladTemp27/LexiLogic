d287806050f40f156b7349ce0cf41199784e9688
Marven <2234244@slu.edu.ph>
2024-05-12 15:35:17 +0800

    + LeaderBoardDAL:   + fetchLeaderBoards
    + GameServiceImpl:
      + getLeaderBoards
    
    + LeaderBoard:
      + added a parameter of rank

--- lexilogicserver/src/main/java/org/amalgam/lexilogicserver/model/serviceimpl/GameServiceImpl.java
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/model/serviceimpl/GameServiceImpl.java
@@ -1,9 +1,16 @@
 package org.amalgam.lexilogicserver.model.serviceimpl;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.amalgam.Service.GameServiceModule.GameServicePOA;
 import org.amalgam.UIControllers.PlayerCallback;
 import org.amalgam.Utils.Exceptions.*;
 import org.amalgam.Utils.Exceptions.DuplicateWordException;
+import org.amalgam.lexilogicserver.model.DAL.LeaderBoardDAL;
+import org.amalgam.lexilogicserver.model.utilities.referenceobjects.LeaderBoard;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class GameServiceImpl extends GameServicePOA {
 
@@ -18,9 +25,30 @@ public class GameServiceImpl extends GameServicePOA {
     }
 
     @Override
-    public String getLeaderboards() throws EmptyLeaderBoardException {
-        return "";
+    public static String getLeaderboards() throws EmptyLeaderBoardException {
+        List<LeaderBoard> leaderboards = LeaderBoardDAL.fetchLeaderBoards();
+
+        if (leaderboards.isEmpty()) {
+            throw new EmptyLeaderBoardException("Leaderboard is empty");
+        }
+
+        JsonArray leaderboardArray = new JsonArray();
+        for (LeaderBoard leaderboard : leaderboards) {
+            JsonObject leaderboardObj = new JsonObject();
+            leaderboardObj.put("username", leaderboard.getUsername());
+            leaderboardObj.put("pts", leaderboard.getPoints());
+            leaderboardObj.put("rank", leaderboard.getRank());
+            leaderboardArray.put(leaderboardObj);
+        }
+
+        JsonObject result = new JsonObject();
+        result.put("object", "leaderboard");
+        result.put("leaderboard", leaderboardArray);
+
+        return result.toString();
     }
+
+
 
     @Override
     public void verifyWord(String word) throws InvalidWordFormatException, DuplicateWordException {
