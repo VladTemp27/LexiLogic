091ca93289448ef48a8b06af40a3fcf3c1ca117e
Lenar <2233894@slu.edu.ph>
2024-05-13 16:55:52 +0800

    + Run ORBD Connections to Server Controller

---
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/runorbd/RunORBDController.java
@@ -0,0 +1,26 @@
+package org.amalgam.lexilogicserver.views.runorbd;
+
+import javafx.fxml.FXML;
+import org.amalgam.lexilogicserver.ServerController;
+
+public class RunORBDController {
+    private ServerController serverController;
+
+    /**
+     * Sets the Main Controller.
+     *
+     * @param mainController
+     */
+    public void setServerController(ServerController mainController) {
+        this.serverController = mainController;
+    }
+
+     /**
+     * Initializes the controller.
+     * This method sets up the UI components and initializes the data model.
+     */
+    @FXML
+    public void initialize() {
+
+    }
+}
