e69a18a1c78f5141c020ea2ddcb65568432e7707
Georcelle <2234664@slu.edu.ph>
2024-05-13 20:46:04 +0800

    + created a controller class for loading page

---
+++ newlexilogicclient/src/main/java/org/amalgam/client/loading/LoadingController.java
@@ -0,0 +1,36 @@
+package org.amalgam.client.loading;
+
+import org.amalgam.client.MainController;
+
+public class LoadingController {
+    private MainController mainController;
+    /**
+     * Sets the Main Controller.
+     *
+     * @param mainController
+     */
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+    /**
+     * Gets the objects used.
+     * This method returns a string indicating the type of objects used by the controller.
+     *
+     * @return A string representing the objects used.
+     */
+    //TODO: @Override
+    private void getObjectsUsed() {
+        //TODO: Return Value
+        //return "user";
+    }
+
+    /**
+     * Fetches and updates data remotely.
+     * This method is called to update the data displayed in the UI.
+     *
+     */
+    //TODO: @Override
+    private void fetchAndUpdate() {
+        //TODO: Fetching of Data
+    }
+}
