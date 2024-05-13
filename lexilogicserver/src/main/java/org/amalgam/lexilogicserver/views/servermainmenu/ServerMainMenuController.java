f08924330546dbbca8fb618a7db2017f5a3489b0
Georcelle <2234664@slu.edu.ph>
2024-05-13 23:26:36 +0800

    + modified the prepared server main menu controller in such a way that the methods are aligning with the calling of different views per button

--- lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/servermainmenu/ServerMainMenuController.java
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/servermainmenu/ServerMainMenuController.java
@@ -1,9 +1,23 @@
 package org.amalgam.lexilogicserver.views.servermainmenu;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import org.amalgam.lexilogicserver.ServerController;
 
 public class ServerMainMenuController {
+    //private variables
+    @FXML
+    private AnchorPane serverMainMenuPane;
+    @FXML
+    private Button addPlayerButton;
+    @FXML
+    private Button changeGameButton;
+    @FXML
+    private Button runORBDButton;
+    @FXML
+    private Button runServerButton;
     private ServerController serverController;
 
     /**
@@ -15,12 +29,87 @@ public class ServerMainMenuController {
         this.serverController = serverController;
     }
 
-     /**
+    /**
+     * Adds hover effect to the given button.
+     *
+     * @param button The button to add hover effect to.
+     */
+    private void addHoverEffect(javafx.scene.control.Button button) {
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#9CA16F, -10%);"));
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #9CA16F;"));
+    }
+
+    /**
+     * Shows an alert to a user if there is an error.
+     *
+     * @param message
+     */
+    private void showAlert(String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    /**
+     * Handles the add player button
+     */
+    @FXML
+    public void handleAddPlayerButton(){
+        if (serverController != null) {
+            serverController.loadAddPlayer();
+        }else {
+            System.out.println("Server controller is not set.");
+        }
+    }
+    /**
+     * Handles the change game option button
+     */
+    @FXML
+    public void handleChangeGameButton(){
+        if (serverController !=null){
+            serverController.loadChangeGame();
+        }else{
+            System.out.println("Server controller is not set.");
+        }
+    }
+    /**
+     * Handles the run ORBD Button
+     */
+    @FXML
+    public void handleRunORBDButton(){
+        if(serverController !=null){
+            serverController.loadRunORBD();
+        }else {
+            System.out.println("Server Controller is not set.");
+        }
+    }
+    /**
+     * Handles the runServer Button
+     */
+    @FXML
+    public void handleRunServerButton(){
+        if(serverController !=null){
+            serverController.startServer();
+        }else {
+            System.out.println("Server controller is not set");
+        }
+    }
+    /**
      * Initializes the controller.
      * This method sets up the UI components and initializes the data model.
      */
     @FXML
     public void initialize() {
+        addHoverEffect(addPlayerButton);
+        addHoverEffect(changeGameButton);
+        addHoverEffect(runORBDButton);
+        addHoverEffect(runServerButton);
+        addPlayerButton.setOnAction(event -> handleAddPlayerButton());
+        changeGameButton.setOnAction(event -> handleChangeGameButton());
+        runORBDButton.setOnAction(event -> handleRunORBDButton());
+        runServerButton.setOnAction(event -> handleRunServerButton());
 
     }
 }
