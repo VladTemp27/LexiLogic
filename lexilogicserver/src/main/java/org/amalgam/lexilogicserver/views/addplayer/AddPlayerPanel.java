2eb0ac4921599c43a8f1e31e88b26df641cd24e4
Lenar <2233894@slu.edu.ph>
2024-05-13 20:22:20 +0800

    + Panel Changes

--- lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/addplayer/AddPlayerPanel.java
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/addplayer/AddPlayerPanel.java
@@ -1,4 +1,11 @@
 package org.amalgam.lexilogicserver.views.addplayer;
 
-public class AddPlayerPanel {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddPlayerPanel extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
 }
+
