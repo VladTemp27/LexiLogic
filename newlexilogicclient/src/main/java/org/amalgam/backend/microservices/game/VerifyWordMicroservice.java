67b1edeef9610409f2ff0194200c61f4965c5aac
Marven <2234244@slu.edu.ph>
2024-05-15 02:00:42 +0800

    + Refactored VerifyWordMicroservice:    + Modified process method into having ORBConnection as a parameter

--- newlexilogicclient/src/main/java/org/amalgam/backend/microservices/game/VerifyWordMicroservice.java
+++ newlexilogicclient/src/main/java/org/amalgam/backend/microservices/game/VerifyWordMicroservice.java
@@ -8,13 +8,7 @@ import org.omg.CosNaming.NamingContextPackage.InvalidName;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 public class VerifyWordMicroservice {
-    private final ORBConnection orbConnection;
-
-    public VerifyWordMicroservice(ORBConnection orbConnection) {
-        this.orbConnection = new ORBConnection(1099, "localhost");
-    }
-
-    public void process(String word){
+    public void process(ORBConnection orbConnection, String word){
         try {
             orbConnection.retrieveGameService().verifyWord(word);
         } catch (InvalidWordFormatException | DuplicateWordException | InvalidName | CannotProceed | NotFound e) {
