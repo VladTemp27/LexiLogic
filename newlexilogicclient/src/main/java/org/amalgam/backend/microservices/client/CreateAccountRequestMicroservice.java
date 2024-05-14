297458397a6a1e43d582950993e7be1b578b9d8e
Marven <2234244@slu.edu.ph>
2024-05-15 02:19:07 +0800

    + Logic into SignUpModel + Removed the initialization of the ORBConnection

--- newlexilogicclient/src/main/java/org/amalgam/backend/microservices/client/CreateAccountRequestMicroservice.java
+++ newlexilogicclient/src/main/java/org/amalgam/backend/microservices/client/CreateAccountRequestMicroservice.java
@@ -8,12 +8,6 @@ import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 public class CreateAccountRequestMicroservice {
 
-    private final ORBConnection orbConnection;
-
-    public CreateAccountRequestMicroservice(ORBConnection orbConnection) {
-        this.orbConnection = new ORBConnection(1099, "localhost");
-    }
-
     public void process (ORBConnection orbConnection, String username, String password)  {
         try {
             orbConnection.retrievePlayerRequestStub().createAccount(username,password);
