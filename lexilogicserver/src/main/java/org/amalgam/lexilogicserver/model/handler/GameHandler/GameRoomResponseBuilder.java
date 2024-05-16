9f2ae1b5f113a13a0946d09b152da10c96dee151
Marven <2234244@slu.edu.ph>
2024-05-16 20:31:11 +0800

    + Refactored GameRoomResponseBuilder

--- lexilogicserver/src/main/java/org/amalgam/lexilogicserver/model/handler/GameHandler/GameRoomResponseBuilder.java
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/model/handler/GameHandler/GameRoomResponseBuilder.java
@@ -1,5 +1,6 @@
 package org.amalgam.lexilogicserver.model.handler.GameHandler;
 
+import com.google.gson.JsonArray;
 import org.amalgam.lexilogicserver.model.utilities.referenceobjects.PlayerGameDetail;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -9,12 +10,35 @@ public class GameRoomResponseBuilder {
 
     private static final Gson gson = new Gson();
 
-    public static String buildGameStartedResponse(LinkedHashMap<String, PlayerGameDetail> details) {
+    public static String buildGameStartedResponse(GameRoom gameRoom) {
+        LinkedHashMap<String, PlayerGameDetail> details = gameRoom.getDetails();
+
         JsonObject response = new JsonObject();
         response.addProperty("state", "game_started");
-        response.add("players", gson.toJsonTree(details));
+
+        JsonObject gameRoomJson = new JsonObject();
+        details.forEach((username, playerDetail) -> {
+            JsonObject playerInfo = new JsonObject();
+            playerInfo.addProperty("username", username);
+            playerInfo.addProperty("points", playerDetail.getPoints());
+            playerInfo.addProperty("ready", playerDetail.isReady());
+
+            JsonArray wordsArray = new JsonArray();
+            playerDetail.getWords().forEach(wordsArray::add);
+            playerInfo.add("words", wordsArray);
+
+            JsonArray dupedWordsArray = new JsonArray();
+            playerDetail.getDupedWords().forEach(dupedWordsArray::add);
+            playerInfo.add("duped_words", dupedWordsArray);
+
+            gameRoomJson.add(username, playerInfo);
+        });
+
+        response.add("game_room", gameRoomJson);
+
         return response.toString();
     }
+
 
     public static String buildStagePlayersResponse(int countdown) {
         JsonObject response = new JsonObject();
