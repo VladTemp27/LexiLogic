310c4c64e50d5902bd406cb8a962633cf0367135
Georcelle <2234664@slu.edu.ph>
2024-05-19 23:37:46 +0800

    + fixed back button implementation

--- lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/changegame/ChangeGameController.java
+++ lexilogicserver/src/main/java/org/amalgam/lexilogicserver/views/changegame/ChangeGameController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import org.amalgam.lexilogicserver.ServerController;
 
@@ -35,9 +37,26 @@ public class ChangeGameController {
      *
      * @param button The button to add hover effect to.
      */
-    private void addHoverEffect(javafx.scene.control.Button button) {
-        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#9CA16F, -10%);"));
-        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #9CA16F;"));
+    private void addHoverEffectImage(Button button) {
+        ImageView imageView = (ImageView) button.getGraphic();
+        ColorAdjust colorAdjust = new ColorAdjust();
+
+        button.setOnMouseEntered(e -> {
+            colorAdjust.setBrightness(-0.3); // Decrease brightness to make it darker
+            imageView.setEffect(colorAdjust);
+        });
+
+        button.setOnMouseExited(e -> {
+            colorAdjust.setBrightness(0); // Reset brightness
+            imageView.setEffect(colorAdjust);
+        });
+    }
+
+    private void addHoverEffect(Button button){
+        String originalColor = button.getStyle(); // Store the original color
+
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(" + originalColor + ", -10%);"));
+        button.setOnMouseExited(e -> button.setStyle(originalColor));
     }
 
     /**
@@ -82,7 +101,7 @@ public class ChangeGameController {
     @FXML
     public void initialize() {
         addHoverEffect(saveButton);
-        addHoverEffect(backButton);
+        addHoverEffectImage(backButton);
         saveButton.setOnAction(event -> handleSaveButton());
         backButton.setOnAction(event -> handleBackButton());
 
