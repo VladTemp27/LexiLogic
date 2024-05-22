960ef510f2727495e9bb46bef50f869e5fd30c2b
Lestat <2233915@slu.edul.ph>
2024-05-22 16:47:27 +0800

    +modified test files to test game cycle

--- newlexilogicclient/src/test/java/MatchMakeTest.java
+++ newlexilogicclient/src/test/java/MatchMakeTest.java
@@ -1,3 +1,7 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.InternalRuntimeError;
 import org.amalgam.Service.GameServiceModule.GameService;
 import org.amalgam.Service.GameServiceModule.GameServiceHelper;
 import org.amalgam.Service.PlayerServiceModule.PlayerService;
@@ -19,6 +23,8 @@ public class MatchMakeTest implements ControllerInterface{
     private GameService gameService;
     private POA rootPOA;
     private CallbackImpl callback;
+    private int gameRoomID;
+    private String currentState = "";
 
     private static Scanner kInput = new Scanner(System.in);
 
@@ -36,6 +42,29 @@ public class MatchMakeTest implements ControllerInterface{
 
         String response = program.gameService.matchMake(PlayerCallbackHelper.narrow(program.rootPOA.servant_to_reference(program.callback)));
         System.out.println(response);
+
+
+        //TODO: move this to ui call, for the most part majority of the bugs are fixed here im gonna sleep
+        // ive been awake for 28 hours right now with 1 month of no actual sleep
+        while(!program.currentState.equals("game_ended")){
+            if(program.currentState.equals("staging")){
+                System.out.println("staging");
+                program.stagingStateHandler();
+                System.out.println(user+" "+program.gameRoomID);
+                program.gameService.playerReady(user, program.gameRoomID);
+                System.out.println("ready sent");
+                program.currentState = "";
+                continue;
+            }
+
+            if(program.currentState.equals("game_started")){
+                program.gameStartedHandler();
+                System.out.println("round done");
+                program.currentState = "";
+                continue;
+            }
+
+        }
     }
 
     public void getAllStubs(){
@@ -48,6 +77,7 @@ public class MatchMakeTest implements ControllerInterface{
 
             playerService = PlayerServiceHelper.narrow(nameService.resolve_str("PlayerService"));
             gameService = GameServiceHelper.narrow(nameService.resolve_str("GameService"));
+
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -68,5 +98,29 @@ public class MatchMakeTest implements ControllerInterface{
     @Override
     public void testUICall(String jsonString) {
         System.out.println(jsonString);
+        JsonElement rootElement = JsonParser.parseString(jsonString);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        gameRoomID = rootObject.get("room_id").getAsInt();
+        currentState = rootObject.get("state").getAsString();
+        System.out.println(gameRoomID);
+
     }
+
+    public void stagingStateHandler(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void gameStartedHandler(){
+        try{
+            //Thread.sleep(000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
