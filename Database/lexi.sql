d4203d2183a3417fe24da3a6de2ee86fdcbcbe6f
Lou <2233672@slu.edu.ph>
2024-05-28 02:10:14 +0800

    Updated .sql database

--- Database/lexi.sql
+++ Database/lexi.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 22, 2024 at 12:08 PM
--- org.amalgam.lexilogicserver.Server.Server version: 8.2.0
+-- Generation Time: May 21, 2024 at 06:29 PM
+-- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -41,8 +41,18 @@ CREATE TABLE IF NOT EXISTS `gamedetails` (
 --
 
 INSERT INTO `gamedetails` (`playerID`, `lobbyID`, `totalPoints`) VALUES
-(1, 101, 50),
-(2, 102, 75);
+(5, 130, 240),
+(4, 127, 640),
+(2, 126, 250),
+(3, 126, 470),
+(1, 125, 640),
+(2, 125, 510),
+(3, 127, 590),
+(6, 128, 530),
+(6, 129, 200),
+(3, 130, 690),
+(4, 128, 650),
+(5, 129, 520);
 
 -- --------------------------------------------------------
 
@@ -52,20 +62,24 @@ INSERT INTO `gamedetails` (`playerID`, `lobbyID`, `totalPoints`) VALUES
 
 DROP TABLE IF EXISTS `leaderboards`;
 CREATE TABLE IF NOT EXISTS `leaderboards` (
-  `leaderBoardID` int NOT NULL,
+  `leaderBoardID` int NOT NULL AUTO_INCREMENT,
   `userID` int DEFAULT NULL,
   `totalPoints` int DEFAULT NULL,
   PRIMARY KEY (`leaderBoardID`),
   KEY `userID` (`userID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `leaderboards`
 --
 
 INSERT INTO `leaderboards` (`leaderBoardID`, `userID`, `totalPoints`) VALUES
-(1, 1, 100),
-(2, 2, 150);
+(31, 6, 730),
+(30, 5, 760),
+(29, 4, 1290),
+(28, 3, 1750),
+(27, 2, 760),
+(26, 1, 640);
 
 -- --------------------------------------------------------
 
@@ -75,19 +89,22 @@ INSERT INTO `leaderboards` (`leaderBoardID`, `userID`, `totalPoints`) VALUES
 
 DROP TABLE IF EXISTS `lobby`;
 CREATE TABLE IF NOT EXISTS `lobby` (
-  `lobbyID` int NOT NULL,
-  `createdBy` varchar(255) DEFAULT NULL,
-  `winner` varchar(255) DEFAULT NULL,
+  `lobbyID` int NOT NULL AUTO_INCREMENT,
+  `winner` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`lobbyID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=131 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `lobby`
 --
 
-INSERT INTO `lobby` (`lobbyID`, `createdBy`, `winner`) VALUES
-(101, 'Marven', 'Luis'),
-(102, 'Luis', 'Marven');
+INSERT INTO `lobby` (`lobbyID`, `winner`) VALUES
+(130, 'Lou'),
+(129, 'Geo'),
+(128, 'Gebreyl'),
+(127, 'Lenar'),
+(126, 'Lou'),
+(125, 'Mark');
 
 -- --------------------------------------------------------
 
@@ -97,21 +114,25 @@ INSERT INTO `lobby` (`lobbyID`, `createdBy`, `winner`) VALUES
 
 DROP TABLE IF EXISTS `player`;
 CREATE TABLE IF NOT EXISTS `player` (
-  `playerID` int NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `playerID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `lastLogin` date DEFAULT NULL,
-  `forDeletion` varchar(255) DEFAULT NULL,
+  `forDeletion` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`playerID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `player`
 --
 
-INSERT INTO `player` (`playerID`, `name`, `password`, `lastLogin`,`forDeletion`) VALUES
-(1, 'Marven', 'password123', '2024-04-21',NULL),
-(2, 'Luis', 'password456', '2024-04-20', NULL);
+INSERT INTO `player` (`playerID`, `name`, `password`, `lastLogin`, `forDeletion`) VALUES
+(1, 'Marven', 'pass123', '2024-05-18', NULL),
+(2, 'Mark', 'pass123', '2024-05-20', NULL),
+(3, 'Lou', 'pass123', '2024-05-06', NULL),
+(4, 'Lenar', 'pass123', '2024-05-16', NULL),
+(5, 'Geo', 'pass123', '2024-05-19', NULL),
+(6, 'Gebreyl', 'pass123', '2024-05-12', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
